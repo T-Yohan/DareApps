af7b745e705f4eb97514f2af6cb1fd92c4ecdf2d
T-Yohan <yohanturlet@outlook.fr>
2023-09-29 12:44:29 -0400

    sliders

--- src/component/ItemCategory.jsx
+++ src/component/ItemCategory.jsx
@@ -1,14 +1,30 @@
 import { useNavigation } from '@react-navigation/native';
 import * as React from 'react';
-import {List} from 'react-native-paper';
+import {List, Text} from 'react-native-paper';
+import styles from '../../styles';
+import { Image, View } from 'react-native';
 
 const ItemCategory = ({category}) => {
     const navigation = useNavigation();
-    return(
-    <List.Item
-    title={category.name}
-    onPress={()=>navigation.navigate('TruthOrDare',{id: category.id})}
-    />
-)};
+//     return(
+//     <List.Item
+//     title={category.name}
+//     onPress={()=>navigation.navigate('TruthOrDare',{id: category.id})}
+//     />
+// )
+console.log(category)
+return (
+    <View style={[
+        styles.slide,
+        {
+            backgroundColor:category.color
+        }
+        ]}>
+        <Text style={styles.slideTitle}>{category.name}</Text>
+        {/* <Image source={{uri: category.image}} /> */}
+        <Text style={styles.slideText}>{category.description}</Text>
+    </View>
+);
+};
 
 export default ItemCategory;
